bff5122df0a8a81a0e7cc7b92ec609507739bcce
Edwin Cloud <ecloud412@gmail.com>
2019-02-14 19:16:30 -0800

    Add solution to alien dictionary

---
+++ LeetCode/Alien Dictionary/README.md
@@ -0,0 +1,42 @@
+# Alien Dictionary
+There is a new alien language which uses the latin alphabet. However, the order among letters are unknown to you. You receive a list of non-empty words from the dictionary, where words are sorted lexicographically by the rules of this new language. Derive the order of letters in this language.
+
+## Example 1:
+Given the following words in dictionary,
+```python
+[
+  "wrt",
+  "wrf",
+  "er",
+  "ett",
+  "rftt"
+]
+```
+The correct order is: `"wertf"`.
+
+## Example 2:
+Given the following words in dictionary,
+```python
+[
+  "z",
+  "x"
+]
+```
+The correct order is: `"zx"`.
+
+## Example 3:
+Given the following words in dictionary,
+```python
+[
+  "z",
+  "x",
+  "z"
+]
+```
+The order is invalid, so return `""`.
+
+## Note:
+1. You may assume all letters are in lowercase.
+2. You may assume that if a is a prefix of b, then a must appear before b in the given dictionary.
+3. If the order is invalid, return an empty string.
+4. There may be multiple valid order of letters, return any one of them is fine.
---
+++ LeetCode/Alien Dictionary/solution.py
@@ -0,0 +1,81 @@
+# overall time complexity : 0(n)
+
+
+def alien_dict(words):
+
+    # initialize a dictionary to hold the letters than follow each letter in list of words
+    graph = {}
+
+    # initialize 0 filled list of alphabet positions
+    alphaPositions = [0] * 26
+
+    # initialize a queue we will use to sort the letters once we establish relationships
+    queue = []
+
+    # initialize a list we will use to build our result string
+    result = []
+
+    # populate graph with each letter maping to an empty set that will hold relations
+    for word in words:
+        for c in word:
+            graph[c] = set()
+
+    # find the letters that follow each letter in our graph
+    for i in range(1, len(words)):
+        for j in range(min(len(words[i-1]), len(words[i]))):
+            if words[i-1][j] != words[i][j]:
+                if words[i][j] in graph[words[i-1][j]]:
+                    break
+                graph[words[i-1][j]].add(words[i][j])
+
+                # mark each alpha position visited
+                alphaPositions[ord(words[i][j]) - 97] += 1
+
+    # add c in graph to queue if it has not been visited
+    for c in graph:
+        if alphaPositions[ord(c) - 97] == 0:
+            queue.append(c)
+
+    # go through queue, building result string
+    while len(queue) > 0:
+        c = queue.pop(0)
+        result.append(c)
+
+        # mark neighbors of current queue item as visited,
+        # if neighbor was previously visited then add it
+        # to the end of the queue
+        for neighbor in graph[c]:
+            alphaPositions[ord(neighbor)-97] -= 1
+            if alphaPositions[ord(neighbor)-97] == 0:
+                queue.append(neighbor)
+
+    # join result to string and return
+    return "".join(result)
+
+
+## TEST ##
+tests = [
+    [
+        "wrt",
+        "wrf",
+        "er",
+        "ett",
+        "rftt"
+    ],
+    [
+        "z",
+        "x"
+    ],
+    [
+        "z",
+        "x",
+        "z"
+    ]
+]
+for test in tests:
+    for i, line in enumerate(test, 1):
+        if i == len(test):
+            print('"', line, '"]')
+        else:
+            print('["', line, '",')
+    print("The correct order is :", alien_dict(test))
